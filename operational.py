f53ad788d8c70b942b5dd83438da4f33775498bf
granantuin <49106818+granantuin@users.noreply.github.com>
2023-07-11 11:31:48 +0200

    Update operational.py

--- operational.py
+++ operational.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
-import requests
 import json
 import plotly.express as px
 from sklearn.metrics import accuracy_score
@@ -13,9 +12,42 @@ from datetime import timedelta
 from sklearn.metrics import accuracy_score
 import sklearn
 import seaborn as sns
+from lightgbm.sklearn import LGBMRegressor
+from datetime import timedelta
+import requests
+import time
+import datetime
+
+
+
+
 
 st.set_page_config(page_title="Corón Machine Learning forecast",layout="wide")
 st.write("## **All times UTC**")
+
+def get_wind():
+  #get actual des dir
+  des_dir = requests.get("https://servizos.meteogalicia.gal/mgrss/observacion/ultimos10minEstacionsMeteo.action?idEst=10085&idParam=DV_SD_10m")
+  json_data = json.loads(des_dir.content)
+  instant = json_data['listUltimos10min'][0]['instanteLecturaUTC']
+  des_dir = json_data['listUltimos10min'][0]['listaMedidas'][0]['valor']
+
+  #get actual dir
+  dir = requests.get("https://servizos.meteogalicia.gal/mgrss/observacion/ultimos10minEstacionsMeteo.action?idEst=10085&idParam=DV_AVG_10m")
+  json_data = json.loads(dir.content)
+  dir = json_data['listUltimos10min'][0]['listaMedidas'][0]['valor']
+
+  #get actual mod
+  mod = requests.get("https://servizos.meteogalicia.gal/mgrss/observacion/ultimos10minEstacionsMeteo.action?idEst=10085&idParam=VV_AVG_10m")
+  json_data = json.loads(mod.content)
+  mod = json_data['listUltimos10min'][0]['listaMedidas'][0]['valor']*1.94384
+
+  #get actual des mod
+  des_mod = requests.get("https://servizos.meteogalicia.gal/mgrss/observacion/ultimos10minEstacionsMeteo.action?idEst=10085&idParam=VV_SD_10m")
+  json_data = json.loads(des_mod.content)
+  des_mod = json_data['listUltimos10min'][0]['listaMedidas'][0]['valor']*1.94384
+  
+  return instant, dir, des_dir,mod, des_mod
 
 def get_meteogalicia_model_4Km(coorde):
     """
@@ -756,6 +788,85 @@ st.write("Better machine learning outcome: {}".format(score_ml))
 
 st.write("Project [link](https://github.com/granantuin/Coron)")
 
+#real time
+switch_state = False
+switch_state = st.sidebar.checkbox("Real data")
+
+if switch_state:
+    #load algorithm file gust
+    algo_rdir_d0 = pickle.load(open("algorithms/rdir_coron_d0.al","rb"))
+    algo_sddir_d0 = pickle.load(open("algorithms/sddir_coron_d0.al","rb"))
+    algo_rspd_d0 = pickle.load(open("algorithms/rspd_coron_d0.al","rb"))
+    algo_sdspd_d0 = pickle.load(open("algorithms/sdspd_coron_d0.al","rb"))
 
 
+    meteo_model = get_meteogalicia_model_4Km(algo_rdir_d0["coor"])
+
+    #add time variables
+    meteo_model["hour"] = meteo_model.index.hour
+    meteo_model["month"] = meteo_model.index.month
+    meteo_model["dayofyear"] = meteo_model.index.dayofyear
+    meteo_model["weekofyear"] = meteo_model.index.isocalendar().week.astype(int)
+
+
+    #select x _var
+    model_x_var_rdir = meteo_model[:24][algo_rdir_d0["x_var"]]
+    model_x_var_sddir = meteo_model[:24][algo_sddir_d0["x_var"]]
+    model_x_var_rspd = meteo_model[:24][algo_rspd_d0["x_var"]]
+    model_x_var_sdspd = meteo_model[:24][algo_sdspd_d0["x_var"]]
+
+    #forecast machine learning wind
+    rdir = algo_rdir_d0["pipe"].predict(model_x_var_rdir)
+    sddir = algo_sddir_d0["pipe"].predict(model_x_var_sddir)
+    rspd = algo_rspd_d0["pipe"].predict(model_x_var_rspd)
+    sdspd = algo_sdspd_d0["pipe"].predict(model_x_var_sdspd)
+
+
+    instant, dir, des_dir,mod, des_mod = get_wind()
+    dir_o, spd_o,time_now,dir_f,spd_f = [],[],[],[],[]
+
+    c=0
+    total_count= st.sidebar.number_input("intervals numbers", value=15, step=5)
+    while c<total_count:
+  
+      #Actual data
+      #if((datetime.datetime.utcnow()-datetime.datetime.strptime(instant, '%Y-%m-%dT%H:%M:%S')).total_seconds()/60)>15:
+        #instant, dir, des_dir,mod, des_mod = get_wind()
+
+      dir_oi = abs(np.rint(np.random.normal(dir, des_dir)))
+      if dir_oi> 360:
+        dir_oi = dir_oi-360
+      dir_o.append(dir_oi)
+
+      spd_o.append(abs(round(np.random.normal(mod, des_mod),0)))
+
+      time_now.append(datetime.datetime.utcnow().strftime("%H:%M:%S"))
+      next_hour = (datetime.datetime.utcnow() + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+    
+      #machine learning forecast
+
+      dir_fi = abs(np.rint(np.random.normal(rdir[next_hour.hour-1], sddir[next_hour.hour-1])))
+      if dir_fi> 360:
+        dir_fi = dir_fi-360
+      dir_f.append(dir_fi)
+      spd_f.append(abs(np.rint(np.random.normal(rspd[next_hour.hour-1], sdspd[next_hour.hour-1])*1.94384)))
+
+      st.write(f"\rtime:",time_now[-1],"//","dir_f:",dir_f[-1],"dir_o:",dir_o[-1],"spd_f:",spd_f[-1],"spd_o:",spd_o[-1])
+      c+=1
+      time.sleep(3)
+
+
+    df_wind = pd.DataFrame({"dir_f": dir_f,
+                              "spd_f": spd_f,
+                              "dir_o":dir_o,
+                              "spd_o":spd_o},
+                             index = time_now)  
+
+    fig, ax = plt.subplots(figsize=(10,6))
+    df_wind[['dir_f',"dir_o"]].plot(grid=True, ax=ax, color = ["b","g"],title="time last measure: "+str(instant));
+    st.pyplot(fig)
+
+    fig, ax = plt.subplots(figsize=(10,6))
+    df_wind[['spd_f',"spd_o"]].plot(grid=True,ax=ax, color = ["b","g"],title="time last measure: "+str(instant));
+    st.pyplot(fig)
 
